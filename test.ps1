5320af00bf2b5b7ecda35f4208c56fbca9244672
Atharva Gandhi <gandhiatharv565@gmail.com>
2026-07-17 22:56:53 +0530

    Chore: Remove legacy scratch scripts and dead patch files from backend

--- test.ps1
+++
@@ -1,3 +0,0 @@
-$json = '{"case_id": "TEST-01", "case_type": "cheque_bounce", "cheque_present": true, "notice_sent": true, "amount": 500000.0, "description": "test"}'
-$res = Invoke-RestMethod -Uri 'https://cheque-bounce-ragbased.onrender.com/api/v1/analyze' -Method Post -Body $json -ContentType 'application/json'
-$res.issues | ConvertTo-Json -Depth 5
